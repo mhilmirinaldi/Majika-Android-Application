9729f126197464c23794554a0feddd8589ffa820
Mahesa Lizardy <88304455+lizardyy@users.noreply.github.com>
2023-02-21 15:28:43 +0700

    fix : change method capture , feat: add more twibbon

--- app/src/main/java/com/example/majika/ui/twibbon/TwibbonFragment.kt
+++ app/src/main/java/com/example/majika/ui/twibbon/TwibbonFragment.kt
@@ -14,16 +14,14 @@ import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.camera.core.CameraSelector
-import androidx.camera.core.ImageCapture
-import androidx.camera.core.ImageCaptureException
-import androidx.camera.core.Preview
+import androidx.camera.core.*
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.example.majika.R
 import com.example.majika.databinding.FragmentTwibbonBinding
 import java.io.ByteArrayOutputStream
+import java.nio.ByteBuffer
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 
@@ -35,6 +33,7 @@ class TwibbonFragment : Fragment() {
     private lateinit var imageCapture: ImageCapture
     private lateinit var safeContext: Context
     private var takePicture = true
+    private var idxImageTwibbon = 0;
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
@@ -60,16 +59,37 @@ class TwibbonFragment : Fragment() {
             takePhoto()
         }
 
+        binding.rightButton.setOnClickListener {
+            changeTwibbon(true);
+        }
+        binding.leftButton.setOnClickListener {
+            changeTwibbon(false);
+        }
+
         return root
     }
 
+    private fun changeTwibbon(right: Boolean){
+        val twibbon :ImageView = binding.twibbon
+        val array = arrayOf(R.drawable.twibbon1,R.drawable.twibbon2,R.drawable.twibbon3,R.drawable.twibbon4,R.drawable.twibbon5,R.drawable.twibbon6,R.drawable.twibbon7,R.drawable.twibbon8,R.drawable.twibbon9)
+        if (right){
+            idxImageTwibbon =(idxImageTwibbon +1 +array.size) % array.size
+        }
+        else {
+            idxImageTwibbon = (idxImageTwibbon - 1 + array.size) % array.size
+        }
+        activity?.runOnUiThread(java.lang.Runnable {
+            twibbon.setImageResource(array[idxImageTwibbon])
+
+        })
+    }
     private fun takePhoto(){
-        val imageOutput: ImageView = binding.imageOutput
+//        val imageOutput: ImageView = binding.imageOutput
         val textTakePicture: TextView = binding.textTakePicture
         if(!takePicture){
             activity?.runOnUiThread(java.lang.Runnable {
-                imageOutput.visibility = View.GONE
-                imageOutput.setImageBitmap(null)
+//                imageOutput.visibility = View.GONE
+//                imageOutput.setImageBitmap(null)
                 textTakePicture.setText(R.string.on_take)
             })
             takePicture = true
@@ -78,39 +98,59 @@ class TwibbonFragment : Fragment() {
         else{
             val outputStream = ByteArrayOutputStream()
             // Create an OutputFileOptions object with the ByteArrayOutputStream
-            val outputFileOptions = ImageCapture.OutputFileOptions.Builder(outputStream).build()
-            imageCapture.takePicture(outputFileOptions, cameraExecutor,
-                object : ImageCapture.OnImageSavedCallback {
-                    override fun onError(error: ImageCaptureException)
-                    {
+//            val outputFileOptions = ImageCapture.OutputFileOptions.Builder(outputStream).build()
+            imageCapture.takePicture(cameraExecutor,
+                object : ImageCapture.OnImageCapturedCallback() {
+                    override fun onCaptureSuccess(image: ImageProxy){
+                        val bitmap = imageProxyToBitmap(image)
+                        super.onCaptureSuccess(image);
 
                     }
-                    override fun onImageSaved(outputFileResults: ImageCapture.OutputFileResults) {
 
-                        activity?.runOnUiThread(java.lang.Runnable {
-                            imageOutput.visibility = View.VISIBLE
-                            textTakePicture.setText(R.string.no_take)
-                            val byteArray = outputStream.toByteArray()
-
-                            // Create a Bitmap from the byte array
-                            val bitmap = BitmapFactory.decodeByteArray(byteArray, 0, byteArray.size)
-
-                            //flip image
-                            val matrix = Matrix()
-                            matrix.preScale(-1f, 1f)
-                            val flipBitmap = Bitmap.createBitmap(bitmap, 0, 0, bitmap.width, bitmap.height, matrix, true)
-                            // Display the Bitmap in the ImageView
-                            imageOutput.setImageBitmap(flipBitmap)
-
-                        })
-
+                    override fun onError(exception: ImageCaptureException) {
+                        super.onError(exception)
                     }
+
+//                object : ImageCapture.OnImageSavedCallback {
+//                    override fun onError(error: ImageCaptureException)
+//                    {
+//
+//                    }
+//
+//
+//                    override fun onImageSaved(outputFileResults: ImageCapture.OutputFileResults) {
+//
+//                        activity?.runOnUiThread(java.lang.Runnable {
+//                            imageOutput.visibility = View.VISIBLE
+//                            textTakePicture.setText(R.string.no_take)
+//                            val byteArray = outputStream.toByteArray()
+//
+//                            // Create a Bitmap from the byte array
+//                            val bitmap = BitmapFactory.decodeByteArray(byteArray, 0, byteArray.size)
+//
+//                            //flip image
+//                            val matrix = Matrix()
+//                            matrix.preScale(-1f, 1f)
+//                            val flipBitmap = Bitmap.createBitmap(bitmap, 0, 0, bitmap.width, bitmap.height, matrix, true)
+//                            // Display the Bitmap in the ImageView
+//                            imageOutput.setImageBitmap(flipBitmap)
+//
+//                        })
+//
+//                    }
                 })
             takePicture = false
             stopCamera()
         }
     }
 
+    private fun imageProxyToBitmap(image: ImageProxy) : Bitmap{
+        val planeProxy = image.planes[0]
+        val buffer: ByteBuffer = planeProxy.buffer
+        val bytes = ByteArray(buffer.remaining())
+        buffer.get(bytes)
+        return BitmapFactory.decodeByteArray(bytes,0, bytes.size)
+    }
 
     override fun onDestroyView() {
         super.onDestroyView()
